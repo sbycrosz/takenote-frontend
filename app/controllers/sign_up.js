e612592aeb568f3e8f799271f1379709dbeca40d
sambya <sambya@icehousecorp.com>
2014-01-21 14:27:51 +0700

    Added sign up logic

---
+++ app/controllers/sign_up.js
@@ -0,0 +1,38 @@
+import ajax from "appkit/utils/ajax";
+import notif from "appkit/utils/notification";
+
+export default Ember.Controller.extend({
+  needs: "application",
+
+  init: function() {
+    this._super();
+    if (window.ENV.debug){
+      this.set('name', 'Sambya');
+      this.set('email', 'sambya@aryasa.net');
+      this.set('password', 'Password01');
+      this.set('password_confirmation', 'Password01');
+    }
+  },
+  
+  errors: null,
+
+  actions:{
+    signUp: function(){
+      var _this = this;
+      var params = this.getProperties('name', 'email', 'password', 'password_confirmation');
+      this.set('errors', null);
+
+      var promise = ajax.post('/sign_up', params);
+      promise.done(function(result){
+        notif.success('Signed up succesfully');
+        var appController = _this.get('controllers.application');
+        appController.setSession(result);
+        _this.transitionToRoute('index');
+      });
+      promise.fail(function(reason){
+        _this.set('errors', reason.responseJSON.error);
+      });
+    }
+  }
+});
+
