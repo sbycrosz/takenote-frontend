a02acb44911e9ea5ede88aec23d877020bf61e46
sambya <sambya@icehousecorp.com>
2014-01-23 10:37:42 +0700

    Handle not found note

--- app/routes/note.js
+++ app/routes/note.js
@@ -1,7 +1,14 @@
 import AuthenticatedRoute from "appkit/routes/authenticated_route";
+import notif from "appkit/utils/notification";
 
 export default AuthenticatedRoute.extend({
   model: function(params){
     return this.modelFor('notes').findBy('id', +params.id);
+  },
+  afterModel: function(model, transition) {
+    if (!model) {
+      notif.error('Not found :(');
+      this.transitionTo('index');
+    }
   }
 });
